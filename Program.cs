3853f78e308a6a62b2316f33bdbfc9054db682c3
Giacomo Scroccaro <info@quidsicurezza.it>
2022-01-09 19:09:13 +0100

    first implementation

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace Slot_Machine
     {
         static void Main(string[] args)
         {
+            int[,] slot = new int[3, 3];
+            int betting = 0;
+            int min=1;
+            int max = 3;
+            Random randNum = new Random();
+                       for (int x = 0; x < slot.GetLength(0); x += 1)
+            {
+                for (int y = 0; y < slot.GetLength(1); y += 1)
+                {
+                    Console.Write(slot[x, y]);
+                }
+            }
+           
         }
     }
 }
