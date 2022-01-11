a2788d9382308cfb6495481abf500322f4107eb3
Giacomo Scroccaro <info@quidsicurezza.it>
2022-01-11 20:28:39 +0100

    first jagged array implementation fixed for loop for printing numbers

--- Program.cs
+++ Program.cs
@@ -10,22 +10,68 @@ namespace Slot_Machine
     {
         static void Main(string[] args)
         {
-            int[,] slot = new int[3, 3];
-            int betting = 0;
+            //int[,] slot = new int[3, 3];
+            int[] slot = new int[3];
+            int[][] jArray1 = new int[2][];
             int min = 1;
             int max = 3;
-            Random randNum = new Random();
-            //use for loops to fill the array with random numbers
+            int numberChoosen = '0';
+            string bettingstring = "";
+            int bettingint = 0;
+            char bettingchar = '0';
+            int prize = 0;
+            int[] combination1 = new int[3] { 1, 1, 1 };
+            int[] combination2 = new int[3] { 2, 2, 2 };
+            int[] combination3 = new int[3] { 3, 3, 3 };
+            int[] Output1 = new int[3];
+            int[] Output2 = new int[3];
+            int[] Output3 = new int[3];
 
-            for (int x = 0; x < slot.GetLength(0); x++)
+            jArray1[0] = Output1;
+            jArray1[1] = Output2;
+            Console.WriteLine(jArray1[0][1]);
+;           int counter = 0;
+            Random randNum = new Random();
+            Console.WriteLine("Do the bet!");
+            bettingstring = Console.ReadLine();
+            while (counter < 2)
             {
-                for (int y = 0; y < slot.GetLength(1); y++)
+                for (int i = 0; i < 3; i++)
                 {
-                    Console.Write(slot[x, y]);
+                    slot[i] = randNum.Next(min, max);
+                    Output1[i] = slot[i];
+                    for (int j = 0; j < slot.Length; j++) //print 1d array
+                    {
+                        Console.Write($"{slot[j]}");
+                    }
+                    counter++;
                 }
                 Console.WriteLine();
             }
+            //            for (int x = 0; x < slot.GetLength(0); x++)
+            //            {
+            //                for (int y = 0; y < slot.GetLength(1); y++)
+            //                {
+            //                    slot[x, y] = randNum.Next(max);
+            //                    Console.Write(slot[x, y]);
 
+            //                }
+            //                if (combination1 == Output)
+            //                {
+            //                    bettingint *= 3;
+            //                }
+            //                else if (combination2 == Output)
+            //                {
+            //                    bettingint *= 6;
+            //                }
+            //                else if (combination3 == Output)
+            //                {
+            //                    bettingint *= 9;
+            //                }
+            //                Console.WriteLine();
+
+            //            }
         }
     }
 }
+
