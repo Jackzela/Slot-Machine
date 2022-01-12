35efd0c7f0c3648c1aec689701f70cd6cbae6abb
Giacomo Scroccaro <info@quidsicurezza.it>
2022-01-12 21:11:51 +0100

    I've created a for loops that assign the random slot number to the jagged array's array.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,10 @@ namespace Slot_Machine
         {
             //int[,] slot = new int[3, 3];
             int[] slot = new int[3];
-            int[][] jArray1 = new int[2][];
+            int[] Output1 = new int[3];
+            int[] Output2 = new int[3];
+            int[] Output3 = new int[3];
+            int[][] jArray1 = new int[3][] {Output1, Output2, Output3};
             int min = 1;
             int max = 3;
             int numberChoosen = '0';
@@ -23,10 +26,6 @@ namespace Slot_Machine
             int[] combination1 = new int[3] { 1, 1, 1 };
             int[] combination2 = new int[3] { 2, 2, 2 };
             int[] combination3 = new int[3] { 3, 3, 3 };
-            int[] Output1 = new int[3];
-            int[] Output2 = new int[3];
-            int[] Output3 = new int[3];
-
             jArray1[0] = Output1;
             jArray1[1] = Output2;
             Console.WriteLine(jArray1[0][1]);
@@ -34,17 +33,16 @@ namespace Slot_Machine
             Random randNum = new Random();
             Console.WriteLine("Do the bet!");
             bettingstring = Console.ReadLine();
-            while (counter < 2)
+            for (int i = 0; i < 3; i++)
             {
-                for (int i = 0; i < 3; i++)
+                for (int y = 0; y < 3; y++)
                 {
                     slot[i] = randNum.Next(min, max);
-                    Output1[i] = slot[i];
+                    jArray1[i][i] = slot[i];
                     for (int j = 0; j < slot.Length; j++) //print 1d array
                     {
                         Console.Write($"{slot[j]}");
                     }
-                    counter++;
                 }
                 Console.WriteLine();
             }
