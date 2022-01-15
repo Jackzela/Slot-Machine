45e5f2029757696372bdda4e42259fa74e75a68f
Giacomo Scroccaro <info@quidsicurezza.it>
2022-01-15 16:32:15 +0100

    Created the conditions command

--- Program.cs
+++ Program.cs
@@ -15,6 +15,10 @@ namespace Slot_Machine
             int[] Output2 = new int[3];
             int[] Output3 = new int[3];
             int[][] jArray1 = new int[3][] { Output1, Output2, Output3 };
+            int[] combination1 = new int[3] { 1, 1, 1 };
+            int[] combination2 = new int[3] { 2, 2, 2 };
+            int[] combination3 = new int[3] { 3, 3, 3 };
+            int[][] jArray2 = new int[3][] { combination1, combination2, combination3 };
             int min = 1;
             int max = 3;
             int numberChoosen = '0';
@@ -22,74 +26,41 @@ namespace Slot_Machine
             int bettingint = 0;
             char bettingchar = '0';
             int prize = 0;
-            int[] combination1 = new int[3] { 1, 1, 1 };
-            int[] combination2 = new int[3] { 2, 2, 2 };
-            int[] combination3 = new int[3] { 3, 3, 3 };
-            jArray1[0] = Output1;
-            jArray1[1] = Output2;
+            int counter = 0;
             Random randNum = new Random();
             Console.WriteLine("Do the bet!");
             bettingstring = Console.ReadLine();
+            bettingint= int.Parse(bettingstring);
             for (int y = 0; y < 3; y++)
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    slot[i] = randNum.Next(min, max);
-                    jArray1[i][i] = slot[i];
-
-                    Console.Write($"{slot[i]}");
-                }
-                Console.WriteLine();
-
-            }
-            if (combination1 == Output2)
-            {
-                prize = bettingint * 5;
-            }
-            if (combination2 == Output2)
-            {
-                prize = bettingint * 10;
-            }
-            if (combination3 == Output2)
-            {
-                prize = bettingint*15;
-            }
-            if (combination1 == Output1)
-            {
-                prize = bettingint * 1;
-            }
-            if (combination2 == Output1)
-            {
-                prize = bettingint * 3;
-            }
-            if (combination3 == Output1)
-            {
-                prize = bettingint * 5;
-                if (combination1 == Output3)
-            {
-                    prize = bettingint * 3;
-                }
-                if (combination2 == Output3)
-                {
-                    prize = bettingint * 4;
-                }
-                if (combination3 == Output3)
-                {
-                    prize = bettingint * 6;
+                    for (y=0; y < 3;y++)
+                    {
+                        slot[i] = randNum.Next(min, max);
+                        jArray1[i][y] = slot[i];
+                        if (jArray1[i][y]==jArray2[i][y])
+                        {
+                            counter++;
+                        }
+                        if (counter== 3)
+                        {
+                            prize = slot[i] * bettingint;
+                        }
+                        Console.Write($"{slot[i]}");
+                    }
+                    Console.WriteLine();
                 }
             }
-            if (prize != 0)
+              if (counter>3)
             {
-                Console.WriteLine($"{prize}");
+                Console.WriteLine($"You Win {prize}");
             }
-            
             else
             {
-                Console.WriteLine("You Lose!");
+                Console.WriteLine("You Lose");
             }
-
         }
     }
 }
 
-         
